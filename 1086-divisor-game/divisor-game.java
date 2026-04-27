350295baffcae79a3891b371a4f3f187153ee87f
Gautham S <133193086+Gautham826@users.noreply.github.com>
2026-04-27 11:42:44 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (52.24%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        int x;
+        if (n % 2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
