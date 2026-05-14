7e814de4c429f4c6ee1e351d59f6d534d074dac8
Gautham S <133193086+Gautham826@users.noreply.github.com>
2026-05-14 09:16:59 +0530

    Time: 5 ms (35.24%) | Memory: 42.9 MB (95.73%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] expected=heights.clone();
+        int[] expected=heights.clone();//clone is used for doing a copy of an array
         Arrays.sort(expected);
         int count=0;
         for(int i=0;i<heights.length;i++){
